6b1475dad554d778348cc328d23b981c88e55221
Darragh Drohan <darragh@li-2042eb4c-24d3-11b2-a85c-ace73e0a87a1.ibm.com>
2025-09-17 13:07:00 +0100

    Add skip RPATH

--- buildscripts/manylinux/build_llvmdev.sh
+++ buildscripts/manylinux/build_llvmdev.sh
@@ -15,4 +15,4 @@ else
 fi
 conda activate buildenv
 conda list
-conda-build /root/llvmlite/conda-recipes/llvmdev_for_wheel --output-folder=/root/llvmlite/docker_output
+conda-build /root/llvmlite/conda-recipes/llvmdev_for_wheel --output-folder=/root/llvmlite/docker_output -- -- -DCMAKE_SKIP_RPATH=ON
